a5d4818e398c6f849fd3386531f2c1b93811bd0d
Dave Mag <davemagarian@Daves-MacBook-Pro-2.local>
2020-05-31 19:42:45 -0700

    Updates

---
+++ precache-manifest.99cdd8e99ff3c736127044b853b810c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de92525eebca3a5e917",
+    "url": "/meme-generator-js/static/css/main.a8c57afc.chunk.css"
+  },
+  {
+    "revision": "7de92525eebca3a5e917",
+    "url": "/meme-generator-js/static/js/main.7de92525.chunk.js"
+  },
+  {
+    "revision": "ba1e03232bffe4fb2a6f",
+    "url": "/meme-generator-js/static/css/1.3a51bfff.chunk.css"
+  },
+  {
+    "revision": "ba1e03232bffe4fb2a6f",
+    "url": "/meme-generator-js/static/js/1.ba1e0323.chunk.js"
+  },
+  {
+    "revision": "04a900ad38a33e5538ae",
+    "url": "/meme-generator-js/static/js/runtime~main.04a900ad.js"
+  },
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/meme-generator-js/static/media/impact.8fc622c3.ttf"
+  },
+  {
+    "revision": "a051c995c4976d67927c6fdd3a07158f",
+    "url": "/meme-generator-js/index.html"
+  }
+];
